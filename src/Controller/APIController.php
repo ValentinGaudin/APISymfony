665cd804137f71aa7f521c8ba7b957d402ada3c8
Valentin <valentingaudin@gmail.com>
2022-04-27 13:50:38 +0200

    Add fixtures

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,17 @@ class APIController extends AbstractController
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/APIController.php',
+        ]);
+    }
+
+    #[Route('/contacts', name: 'all_contacts', methods: 'GET')]
+    public function getAllContacts(ContactRepository $contactRepository): Response
+    {
+        $contacts = $contactRepository->findAll();
+
+        return $this->json([
+            'message' => 'Get all contacts from database',
+            'contacts' => $contacts
         ]);
     }
 }
